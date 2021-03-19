a37305c08fb3d90a8edbf9e3eb6ab7958d86c9e5
Daniel Marino <starzonmyarmz@gmail.com>
2021-03-19 18:52:45 -0400

    minify html

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const htmlmin = require("html-minifier")
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles.css")
 
@@ -11,6 +13,19 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("is", (data, letter) => {
     return data.includes(letter) ? 'data-y' : 'data-n'
+  })
+
+  eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+    if (outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        collapseInlineTagWhitespace: true,
+        collapseWhitespace: true,
+        removeComments: true,
+        useShortDoctype: true
+      })
+      return minified
+    }
+    return content
   })
 
   return {
